1acc711828538c5adb8eeb310c12cd6236ff8ca2
dustin-jw <dustin@heysparkbox.com>
2022-05-02 17:00:26 -0600

    chore: set up legacy builds with webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
+
+// return any .js or .ts files matching in the directory (non-recursively)
+const getEntryPoints = (directory) => fs.readdirSync(path.join(__dirname, directory))
+  .filter((file) => !fs.statSync(path.join(directory, file)).isDirectory())
+  .reduce((entries, file) => ({
+    ...entries,
+    [file.split('.')[0]]: `./${directory}/${file}`,
+  }), {});
+
+module.exports = {
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  entry: () => getEntryPoints('src/js'),
+  module: {
+    rules: [
+      {
+        test: /\.(j|t)s$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [
+      '.ts',
+      '.js',
+    ],
+  },
+  output: {
+    filename: '[name].legacy.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
