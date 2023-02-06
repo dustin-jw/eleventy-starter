b5278cfd2e6b7226fd8b2b63a20e4e7cf919d28c
dustin-jw <82047862+dustin-jw@users.noreply.github.com>
2023-02-06 13:59:53 -0700

    feat: replace jest with vitest (#112)
    * chore: install vitest
    
    * chore: remove jest config
    
    * chore: uninstall jest dependencies
    
    * feat: update tests to work with vitest

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.js$': 'babel-jest',
-  },
-};
--- src/js/utilities/divide.test.ts
+++ src/js/utilities/divide.test.ts
@@ -1,3 +1,4 @@
+import { describe, it, expect } from 'vitest';
 import { divide } from './divide';
 
 describe('divide', () => {
