78e5008b114958f262ac2af6117d49edf17d2cf6
dustin-jw <dustin@heysparkbox.com>
2023-02-07 12:03:14 -0700

    chore: run prettier on repo

--- vitest.config.js
+++ vitest.config.js
@@ -2,9 +2,6 @@ import { configDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    exclude: [
-      ...configDefaults.exclude,
-      'e2e-tests/*',
-    ],
+    exclude: [...configDefaults.exclude, 'e2e-tests/*'],
   },
 });
