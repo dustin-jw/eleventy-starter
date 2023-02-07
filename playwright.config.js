ba21e05196f1e4ee383b024c70c31d01fd4ab5e4
dustin-jw <dustin@heysparkbox.com>
2023-02-07 11:23:27 -0700

    chore: limit number of browsers to check to speed up tests

--- playwright.config.js
+++ playwright.config.js
@@ -46,21 +46,7 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-
     /* Test against mobile viewports. */
-    {
-      name: 'Mobile Chrome',
-      use: { ...devices['Pixel 5'] },
-    },
     {
       name: 'Mobile Safari',
       use: { ...devices['iPhone 12'] },
