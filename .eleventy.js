cbee4da4bfb9ce5d34fc8520624a283a6dad7ff7
dustin-jw <dustin@heysparkbox.com>
2023-02-13 08:43:45 -0700

    refactor: handle 404s the Eleventy way

--- .eleventy.js
+++ .eleventy.js
@@ -1,22 +1,6 @@
 const fs = require('fs');
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.setBrowserSyncConfig({
-    callbacks: {
-      ready: function (err, bs) {
-        bs.addMiddleware('*', (req, res) => {
-          const content_404 = fs.readFileSync('dist/404/index.html');
-          // Provides the 404 content without redirect.
-          res.write(content_404);
-          // Add 404 http status code in request header.
-          res.writeHead(404);
-          res.end();
-        });
-      },
-    },
-    files: ['dist/**/*'],
-  });
-
   eleventyConfig.addWatchTarget('./src/scss/');
   eleventyConfig.addWatchTarget('./src/js/');
 
